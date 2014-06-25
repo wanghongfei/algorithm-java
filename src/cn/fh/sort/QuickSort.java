943189f1ffbfd6ce59f1d4adcafe7017f3127604
wanghongfei <brucew@vip.qq.com>
2014-06-25 23:31:04 +0800

    练习快速排序

--- src/cn/fh/sort/QuickSort.java
+++ src/cn/fh/sort/QuickSort.java
@@ -35,12 +35,38 @@ public class QuickSort {
 		
 	}
 	
+	public static void sort2(Comparable[] arr, int start, int end) {
+		if (end <= start) {
+			return;
+		}
+		
+		int low = start;
+		int high = end;
+		Comparable key = arr[low];
+		
+		while (low < high) {
+			while (low < high && key.compareTo(arr[high]) <= 0) {
+				--high;
+			}
+			arr[low] = arr[high];
+			
+			while(low < high && key.compareTo(arr[low]) >= 0) {
+				++low;
+			}
+			arr[high] = arr[low];
+		}
+		
+		arr[low] = key;
+		sort2(arr, start, low - 1);
+		sort2(arr, low + 1, end);
+	}
+	
 	public static void main(String[] args) throws IOException {
 		Integer[] nums = ReadData.readIntFromConsole();
 		
 		Stopwatch w1 = new Stopwatch();
 		w1.start();
-		sort(nums, 0, nums.length - 1);
+		sort2(nums, 0, nums.length - 1);
 		//Arrays.sort(nums);
 		assert Test.isSorted(nums);
 		System.out.println("time : " + w1.elapse());
